20d8d28598a6ea23232cb4cf7959e9c000820085
Jay Brockman <jbb@nd.edu>
2024-01-22 09:08:58 -0500

    Added lecture04

---
+++ lecture04/duality.c
@@ -0,0 +1,33 @@
+/* duality.c */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void dump(int *a, int n) {
+    // WRONG: n = sizeof(a) / sizeof(int);
+    for (size_t i = 0; i < n; i++) {
+    	printf("%ld. %d\n", i, a[i]);
+    }
+    
+    // POINTERS: only
+    // SENTINEL: for (int *p = a; *p >= 0; p++) {
+    for (int *p = a; p < (a + n); p++) {
+    	printf("%ld. %d\n", p - a, *p);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int a[]  = {5, 4, 7, 0, 1, -1};
+    size_t n = sizeof(a) / sizeof(int);
+
+    for (size_t i = 0; i < n; i++) {
+    	assert(a[i]  == *(a + i));
+    	assert(a[i]  == *(i + a));
+    	assert(a[i]  == i[a]);
+	    assert(a + i == &a[i]);
+    }
+
+    dump(a, n);
+    return EXIT_SUCCESS;
+}
---
+++ lecture04/pointers.c
@@ -0,0 +1,50 @@
+/* pointers.c */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])  {
+    int  x = 1;
+    int  y = 2;
+    int *p = NULL;
+
+
+    printf("x = %16d, Address of x = 0x%lx, Size of x = %lu\n", x, (intptr_t)&x, sizeof(x));
+    printf("y = %16d, Address of y = 0x%lx, Size of y = %lu\n", y, (intptr_t)&y, sizeof(y));
+    printf("p = %16lx, Address of p = 0x%lx, Size of p = %lu\n\n", (intptr_t)p, (intptr_t)&p, sizeof(p));
+
+    puts("p = &x");
+    p = &x;
+    printf("x = %16d, Address of x = 0x%lx\n", x, (intptr_t)&x);
+    printf("y = %16d, Address of y = 0x%lx\n", y, (intptr_t)&y);
+    printf("p = %16lx, Address of p = 0x%lx\n", (intptr_t)p, (intptr_t)&p);
+    printf("*p == x ? %d\n", *p == x);
+    printf("*p == y ? %d\n\n", *p == y);
+
+    puts("p--");
+    p--;
+    printf("x = %16d, Address of x = 0x%lx\n", x, (intptr_t)&x);
+    printf("y = %16d, Address of y = 0x%lx\n", y, (intptr_t)&y);
+    printf("p = %16lx, Address of p = 0x%lx\n", (intptr_t)p, (intptr_t)&p);
+    printf("*p == x ? %d\n", *p == x);
+    printf("*p == y ? %d\n\n", *p == y);
+
+    puts("p++");
+    p++;
+    printf("x = %16d, Address of x = 0x%lx\n", x, (intptr_t)&x);
+    printf("y = %16d, Address of y = 0x%lx\n", y, (intptr_t)&y);
+    printf("p = %16lx, Address of p = 0x%lx\n", (intptr_t)p, (intptr_t)&p);
+    printf("*p == x ? %d\n", *p == x);
+    printf("*p == y ? %d\n\n", *p == y);
+
+    puts("p = &y");
+    p = &y;
+    printf("x = %16d, Address of x = 0x%lx\n", x, (intptr_t)&x);
+    printf("y = %16d, Address of y = 0x%lx\n", y, (intptr_t)&y);
+    printf("p = %16lx, Address of p = 0x%lx\n", (intptr_t)p, (intptr_t)&p);
+    printf("*p == x ? %d\n", *p == x);
+    printf("*p == y ? %d\n\n", *p == y);
+
+    return EXIT_SUCCESS;
+}
