838d68209a92ca32676cbc2fc18e33da9b9b4da3
Jay Brockman <jbb@nd.edu>
2024-01-23 20:38:13 -0500

    added regions_sorted

--- lecture05/regions.c
+++ lecture05/regions.c
@@ -15,23 +15,23 @@ int main(int argc, char *argv[]) {
     int      *p2   = malloc(10*sizeof(int));
     static int t   = 2;
 
-    printf("   a. Address: 0x%012lx, Size: %lu\n", (uintptr_t) &a, sizeof( a));
+    printf("   a. Address: 0x%012lx, Size: %lu\n", (uintptr_t) &a, sizeof( a));  // stack
     puts("");
-    printf("   s. Address: 0x%012lx, Size: %lu\n", (uintptr_t)  s, sizeof( s));
-    printf("  &s. Address: 0x%012lx, Size: %lu\n", (uintptr_t) &s, sizeof(&s));
-    printf("  *s. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&*s, sizeof(*s));
+    printf("   s. Address: 0x%012lx, Size: %lu\n", (uintptr_t)  s, sizeof( s));  // data
+    printf("  &s. Address: 0x%012lx, Size: %lu\n", (uintptr_t) &s, sizeof(&s));  // stack
+    printf("  *s. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&*s, sizeof(*s));  // data (same as s)
     puts("");
-    printf("  p0. Address: 0x%012lx, Size: %lu\n", (uintptr_t) p0, sizeof( p0));
-    printf(" &p0. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&p0, sizeof(&p0));
-    printf("  p1. Address: 0x%012lx, Size: %lu\n", (uintptr_t) p1, sizeof( p1));
-    printf(" &p1. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&p1, sizeof(&p1));
-    printf("  p2. Address: 0x%012lx, Size: %lu\n", (uintptr_t) p2, sizeof( p2));
-    printf(" &p2. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&p2, sizeof(&p2));
+    printf("  p0. Address: 0x%012lx, Size: %lu\n", (uintptr_t) p0, sizeof( p0)); // 0
+    printf(" &p0. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&p0, sizeof(&p0)); // stack
+    printf("  p1. Address: 0x%012lx, Size: %lu\n", (uintptr_t) p1, sizeof( p1)); // heap
+    printf(" &p1. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&p1, sizeof(&p1)); // stack
+    printf("  p2. Address: 0x%012lx, Size: %lu\n", (uintptr_t) p2, sizeof( p2)); // heap
+    printf(" &p2. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&p2, sizeof(&p2)); // stack
     puts("");
-    printf("  GD. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&GD, sizeof(GD));
-    printf("   t. Address: 0x%012lx, Size: %lu\n", (uintptr_t) &t, sizeof(t));
+    printf("  GD. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&GD, sizeof(GD));  // data
+    printf("   t. Address: 0x%012lx, Size: %lu\n", (uintptr_t) &t, sizeof(t));   // data
     puts("");
-    printf("main. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&main, sizeof(main));
+    printf("main. Address: 0x%012lx, Size: %lu\n", (uintptr_t)&main, sizeof(main));  // code
 
     /* Demonstrations
      *
