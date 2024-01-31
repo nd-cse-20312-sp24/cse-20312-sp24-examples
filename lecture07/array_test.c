a8146788c345119adead920898134edd0d91b287
Jay Brockman <jbb@nd.edu>
2024-01-31 10:14:51 -0500

    added more printing to array_test.c

--- lecture07/array_test.c
+++ lecture07/array_test.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
     // Create Array
     Array* array = array_create();
 
+    puts("---- Initial size and capacity");
     printf("array->size     = %d\n", array->size);
     printf("array->capacity = %d\n", array->capacity);
 
@@ -20,17 +21,25 @@ int main(int argc, char *argv[]) {
     array_append(array, 101);
     array_append(array, 102);
     array_append(array, 103);
+
+    puts("---- Size and capacity after 4 appends");
+    printf("array->size     = %d\n", array->size);
+    printf("array->capacity = %d\n", array->capacity);
+
     array_append(array, 104);
 
+    puts("---- Size and capacity after 5th append");
     printf("array->size     = %d\n", array->size);
     printf("array->capacity = %d\n", array->capacity);
 
     // Access elements in Array
+    puts("---- Loop over full array using array_at");
     for (int i = 0; i < array->size; i++) {
         printf("array[%d] = %d\n", i, array_at(array, i));
     }
 
     // Search for elements in Array
+    puts("---- Testing array_index");
     printf("array_index(100) = %d\n", array_index(array, 100));
     printf("array_index(102) = %d\n", array_index(array, 102));
     printf("array_index(104) = %d\n", array_index(array, 104));
@@ -41,9 +50,11 @@ int main(int argc, char *argv[]) {
     array_insert(array, 2, 1002);
     array_insert(array, 4, 1003);
 
+    puts("---- After 4 array inserts");
     printf("array->size     = %d\n", array->size);
     printf("array->capacity = %d\n", array->capacity);
 
+    puts("---- Loop over full array using array_at");
     for (int i = 0; i < array->size; i++) {
         printf("array[%d] = %d\n", i, array_at(array, i));
     }
